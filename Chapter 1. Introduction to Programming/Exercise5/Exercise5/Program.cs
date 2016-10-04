7554aac49040ae2c73be9da3240cc5ffc6e4bd11
KarimZida <Zidaka1997@gmail.com>
2016-10-04 20:37:12 +0200

    Exercise5

---
+++ Chapter 1. Introduction to Programming/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Modify the sample program to print a different greeting, for example
+             "Good Day!".*/
+            Console.WriteLine("Good Day!");
+        }
+    }
+}
