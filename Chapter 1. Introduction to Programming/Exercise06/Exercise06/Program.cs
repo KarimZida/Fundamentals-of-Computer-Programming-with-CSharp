0e192e7b26768f40974ddade977da0a034abea90
KarimZida <Zidaka1997@gmail.com>
2016-10-06 08:33:48 +0200

    Exercise06

---
+++ Chapter 1. Introduction to Programming/Exercise06/Exercise06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Exercise06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name ?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name ?");
+            string secondName = Console.ReadLine();
+            Console.WriteLine("Your first name is {0} and your last name is {1}.", firstName, secondName);
+        }
+    }
+}
